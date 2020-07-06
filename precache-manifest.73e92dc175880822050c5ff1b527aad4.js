c90829217a8ca823f7b8a4bf8604ada294b67e34
dartcal <lily.s.maechling.23@dartmouth.edu>
2020-07-05 17:08:39 -0700

    Updates

---
+++ precache-manifest.73e92dc175880822050c5ff1b527aad4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4994c1d70d1bd0a07cecc690c672913b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047c89d9cf0b50567e4c",
+    "url": "/static/css/2.8ba55672.chunk.css"
+  },
+  {
+    "revision": "8d4e88a6980eea86157f",
+    "url": "/static/css/main.d94b6372.chunk.css"
+  },
+  {
+    "revision": "047c89d9cf0b50567e4c",
+    "url": "/static/js/2.deff181f.chunk.js"
+  },
+  {
+    "revision": "6939c1e79ac5456c8b0e7405a79a60d3",
+    "url": "/static/js/2.deff181f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4e88a6980eea86157f",
+    "url": "/static/js/main.2cde50fd.chunk.js"
+  },
+  {
+    "revision": "420ee3e5f7c71d527e09",
+    "url": "/static/js/runtime-main.ba726012.js"
+  },
+  {
+    "revision": "54a72ab5d9547b26b13f7e234ddf1271",
+    "url": "/static/media/dylan.54a72ab5.png"
+  },
+  {
+    "revision": "709eca1a491bebace2dca79ce78c136a",
+    "url": "/static/media/kat.709eca1a.png"
+  },
+  {
+    "revision": "8459843dbead11b0a73e32adb6043216",
+    "url": "/static/media/lily.8459843d.png"
+  },
+  {
+    "revision": "8cc5f8975ba98c62156b96d4952ee203",
+    "url": "/static/media/magnifying-glass.8cc5f897.png"
+  },
+  {
+    "revision": "99b1a065e08ee4e0b042afc91a22a343",
+    "url": "/static/media/scott.99b1a065.png"
+  },
+  {
+    "revision": "58af6f467632a7640855efc360237339",
+    "url": "/static/media/splashBackground.58af6f46.png"
+  },
+  {
+    "revision": "291452def1070e9aa24ace40d235501b",
+    "url": "/static/media/william.291452de.png"
+  }
+]);
